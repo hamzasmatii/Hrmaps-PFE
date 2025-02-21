d2512fa9e416458dfd331244b393f4ef0ac0b95d
hamzasmati <smatihamzaa@gmail.com>
2025-02-21 16:18:09 +0100

    correction front back

--- Front/src/main/java/tn/esprit/artifact/entity/EvaluationType.java
+++
@@ -1,9 +0,0 @@
-package tn.esprit.artifact.entity;
-
-public enum EvaluationType {
-    EXCELLENT,
-    SATISFAISANT,
-    INSATISFAISANT,
-
-    FORUSER
-}
--- Front/src/main/java/tn/esprit/artifact/entity/UserType.java
+++
@@ -1,7 +0,0 @@
-package tn.esprit.artifact.entity;
-
-public enum UserType {
-    ADMIN,
-    CHEF_EQUIPE,
-    EMPLOYE
-}
